b7a6f455cced78b2ee0a601cb30d7b74b6b687b1
Local_PC <sv7230@srmist.edu.in>
2023-09-08 18:13:08 +0530

    Module 1 and Datasets added

--- app.py
+++ app.py
@@ -1,30 +1,65 @@
-# Import necessary libraries
+#pip install streamlit
+#pip install pandas
+#pip install sklearn
+
+
+# IMPORT STATEMENTS
+import streamlit as st
 import pandas as pd
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import seaborn as sns
 
-# Step 1: Data Collection
-# Replace 'data.csv' with the path to your dataset file.
-data = pd.read_csv('data.csv')
 
-# Step 2: Data Preprocessing
-# Handle missing values
-data.dropna(inplace=True)
 
-# Encode categorical variables (if applicable)
-data = pd.get_dummies(data, columns=['categorical_feature'])
+df = pd.read_csv(![](diabetes.csv))
 
-# Split the data into features (X) and target variable (y)
-X = data.drop('diabetes_status', axis=1)
-y = data['diabetes_status']
+# HEADINGS
+st.title('Diabetes Checkup')
+st.sidebar.header('Patient Data')
+st.subheader('Training Data Stats')
+st.write(df.describe())
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize numerical features
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# X AND Y DATA
+x = df.drop(['Outcome'], axis = 1)
+y = df.iloc[:, -1]
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
 
-# Step 6: Data Splitting (Optional)
-# Further split data into training, validation, and testing sets if needed.
+
+# FUNCTION
+def user_report():
+  pregnancies = st.sidebar.slider('Pregnancies', 0,17, 3 )
+  glucose = st.sidebar.slider('Glucose', 0,200, 120 )
+  bp = st.sidebar.slider('Blood Pressure', 0,122, 70 )
+  skinthickness = st.sidebar.slider('Skin Thickness', 0,100, 20 )
+  insulin = st.sidebar.slider('Insulin', 0,846, 79 )
+  bmi = st.sidebar.slider('BMI', 0,67, 20 )
+  dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4, 0.47 )
+  age = st.sidebar.slider('Age', 21,88, 33 )
+
+  user_report_data = {
+      'pregnancies':pregnancies,
+      'glucose':glucose,
+      'bp':bp,
+      'skinthickness':skinthickness,
+      'insulin':insulin,
+      'bmi':bmi,
+      'dpf':dpf,
+      'age':age
+  }
+  report_data = pd.DataFrame(user_report_data, index=[0])
+  return report_data
+
+
+
+
+# PATIENT DATA
+user_data = user_report()
+st.subheader('Patient Data')
+st.write(user_data)
